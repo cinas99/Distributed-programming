df17c6b0bf37f969c97cc02244103a4d8309f991
cinas99 <marcin.konieczny.poznan@gmail.com>
2018-05-25 19:17:55 +0200

    Check of the branch

--- mpi_pthread.cpp
+++ mpi_pthread.cpp
@@ -5,4 +5,5 @@ void sendPacket(packet_t *pkt, int destination, int tag)
       pkt->timestamp = zegarLamporta;
       MPI_Send( pkt, 1, MPI_PAKIET_T, destination, tag, MPI_COMM_WORLD);
     pthread_mutex_unlock(&state_mut);
+/// end
 }
