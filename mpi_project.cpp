a30119324af24387c940563261a118b77272f72d
cinas99 <marcin.konieczny.poznan@gmail.com>
2018-05-25 19:11:32 +0200

    mpi_project with IntoLIFT

--- mpi_project.cpp
+++ mpi_project.cpp
@@ -1,11 +1,15 @@
-#include "mpi.h"
 #include <vector>
+#include "mpi.h"
 #include <cstdio>
+#include <ctime>
 #include <iostream>
+#include <thread>
+
 #define N 150
-#define S 4
+#define S 4 // size
+
+// zadania
 #define W_KOLEJCE 0
-//zadanie
 #define NA_WYCIAGU 1
 #define KONIEC_ZJAZDU 2
 #define MSG_SIZE 4
@@ -42,6 +46,7 @@ void sendMessage(int *msg, struct Narciarz *narciarz){
 	MPI_Bcast(msg, MSG_SIZE, MPI_INT, msg[0], MPI_COMM_WORLD);
 	printf("%d: Wysylam bcast\n", msg[0]);
 }
+
 
 void receiveMessage(int rank) {
 	int msg[MSG_SIZE];
@@ -86,11 +91,70 @@ void sortQueue(){
 		}
 }
 
+void sleepAndAnswer(int sleepTime, struct Narciarz *narciarz, int which){
+
+	 cout<<which<<" Thread - SleepTime:"<<sleepTime<<" Narciarz rank"<<narciarz->rank<<" \n";
+	 // odpowiadaj "Jest na wyciagu" + zapisuj czasy procesow do queue
+   MPI_Status status;
+	 if (narciarz->rank == 0){
+		 cout<<"Jestem 1 *********\n";
+		 int x;
+		 MPI_Request req1;
+		 MPI_Isend(&x ,1,MPI_INT,1,13,MPI_COMM_WORLD, &req1);
+		 MPI_Wait(&req1,&status);
+	 }
+	 else {
+		 cout<<"Jestem 2 ********\n";
+		int data;
+ 	 	MPI_Recv(&data ,1,MPI_INT,0,13,MPI_COMM_WORLD, &status);
+		cout<<"Data recived:"<<data<<" \n";
+
+	 }
+}
+
+void intoLift(struct Narciarz *narciarz){
+		// recivedClock do wywalenia po prawidłowym przekazaniu Queue (wezmiemy czas z niego)
+		int recivedClock = 0;
+		int randTime = 0;
+		int randTime2 = 0;
+
+		srand( time ( NULL));
+		cout<< "iL"<< narciarz->rank <<"\n";
+
+		//wjedz na szczyt -> wait(Randtime) + odpowiadaj w drugm watku "jest na wyciagu" i zapisuj do queue otrzymane czasy
+		randTime = 500 + (rand() % 300);
+		thread t1(sleepAndAnswer,randTime, narciarz, 1);
+		t1.join();
+
+		// zsynchronizuj zegar max((my,odebrane zegary)) i zinkrementuj go
+		if(narciarz->zegar >= recivedClock)
+			narciarz->zegar += 1;
+		else
+			narciarz->zegar = recivedClock +1;
+
+		// Zejscie z wyciagu -> wyslij do wszystkich bcast
+		// TODO BCAST "ZAKONCZONO WJAZD"
+
+		// Zjezdzaj na nartach + Odbieraj wiadomosci od procesow w watku
+		randTime2 = 500 + (rand() % 300);
+		thread t2(sleepAndAnswer,randTime2, narciarz, 2);
+		t2.join();
+		// zsynchronizuj zegar max((my,odebrane zegary)) i zinkrementuj go
+		if(narciarz->zegar >= recivedClock)
+			narciarz->zegar += 1;
+		else
+			narciarz->zegar = recivedClock +1;
+}
+
 
 int main( int argc, char **argv )
 {
 	int zegar = 0;
 	int rank, size;
+
+	// wektor watkow
+	std::vector<std::thread> threads;
+
 	MPI_Init( &argc, &argv );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
@@ -110,12 +174,18 @@ int main( int argc, char **argv )
 	//printf( "Jestem %d z %d na %s\n", rank, size, processor_name );
 	printf( "Jestem narciarzem %d o wadze %d i stanie %d. Moj zegar to %d, nazywam sie %s\n", narciarz->rank, narciarz->waga, narciarz->stan, narciarz->zegar, processor_name);
 
-
 	queue.push_back(narciarz);
 	sendMessage(msg, narciarz);
 	MPI_Barrier(MPI_COMM_WORLD);
 	receiveMessage(rank);
 	MPI_Barrier(MPI_COMM_WORLD);
 	printf("%d: Skonczylem!\n", rank);
+
+	//testuje funkcje wejdz na wyciag po momencie jak wszyscy wymienili sie wiadomosciami
+	if(1){
+		//cout<<"Sekcja Krytyczna procesu: "<<narciarz->rank<<" \n";
+		intoLift(narciarz);
+	}
+
 	MPI_Finalize();
 }
---
+++ mpi_pthread.cpp
@@ -0,0 +1,8 @@
+void sendPacket(packet_t *pkt, int destination, int tag)
+{
+    pthread_mutex_lock(&state_mut);
+      zegarLamporta++;
+      pkt->timestamp = zegarLamporta;
+      MPI_Send( pkt, 1, MPI_PAKIET_T, destination, tag, MPI_COMM_WORLD);
+    pthread_mutex_unlock(&state_mut);
+}
